5d64e1addf8e6b97848783428583b74a070e29c3
Tuan Anh <langthambca@gmail.com>
2022-09-09 06:35:18 +0700

    a

--- src/components/Study.jsx
+++ src/components/Study.jsx
@@ -1,43 +1,48 @@
-import React from 'react';
+import React, { useEffect, useRef } from "react";
 // import Toogle from './Toggle/Index';
 
 export default function Study() {
+  const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current.focus();
+  })
   const [showModal, setShowModal] = React.useState(false);
   const [toggled, setToggled] = React.useState(false);
-  const [showTooleMode, setShowTooleMode] = React.useState(true);
+  const [showFeeback, setShowFeeback] = React.useState(false);
   const [showTimer, setShowTimer] = React.useState(false);
   const handleClick = () => {
     setToggled(!toggled);
   };
   const [inputField, setInputField] = React.useState({
-    task: '',
-    bio: '',
-    tomato: '',
+    task: "enter task",
+    bio: "",
+    tomato: 25,
   });
   const inputsHandler = (e) => {
-    setInputField({ [e.target.name]: e.target.value });
+    setInputField({ [inputRef.current.name]: inputRef.current });
+    console.log(inputRef.current.name, inputRef.current.value);
   };
   const handleSubmit = (e) => {
-    alert(inputField.task);
+    alert(inputField.tomato);
     e.preventDefault();
   };
   const StudyAndMode = () => {
     return (
       <>
         {/* study and mode */}
-        <div className=' text-center'>
-          <h1 className='font-weight-bold display-1'>Study</h1>
+        <div className=" text-center">
+          <h1 className="font-weight-bold display-1">Study</h1>
           <div>
             <label
-              htmlFor='large-toggle'
-              className='inline-flex relative items-center cursor-pointer'
+              htmlFor="large-toggle"
+              className="inline-flex relative items-center cursor-pointer"
             >
               <input
-                type='checkbox'
-                value=''
-                id='large-toggle'
+                type="checkbox"
+                value=""
+                id="large-toggle"
                 // checked
-                className='sr-only peer'
+                className="sr-only peer"
               />
             </label>
             <Toogle />
@@ -47,109 +52,70 @@ export default function Study() {
       </>
     );
   };
-  const Modal = () => {
+  const FormTask = () => {
     return (
-      <div>
-        <div
-          className='modal fade '
-          id='exampleModal'
-          tabIndex='-1'
-          aria-labelledby='exampleModalLabel'
-          aria-hidden='true'
-        >
-          <div className='modal-dialog '>
-            <div className='modal-content'>
-              <div className='modal-header bg-secondary text-white'>
-                <h5
-                  className='modal-title'
-                  id='exampleModalLabel'
-                >
-                  new task
-                </h5>
-                <button
-                  type='button'
-                  className='btn-close'
-                  data-bs-dismiss='modal'
-                  aria-label='Close'
-                ></button>
-              </div>
-              {/* body */}
-              <div className='modal-body bg-info text-dark'>
-                <form
-                  className=' p-3'
-                  action=''
-                  onSubmit={handleSubmit}
-                >
-                  <div>
-                    <input
-                      type='text'
-                      placeholder='your task...'
-                      name='task'
-                      onChange={(e) => inputsHandler(e)}
-                    />{' '}
-                    <br />
-                    <input
-                      onChange={inputsHandler}
-                      defaultChecked
-                      type='radio'
-                      name='bio'
-                      id='home'
-                    />
-                    <label htmlFor='home'>Home</label>
-                    <input
-                      onChange={inputsHandler}
-                      type='radio'
-                      name='bio'
-                      id='Office'
-                    />
-                    <label htmlFor='Office'>Office</label>
-                    <input
-                      onChange={inputsHandler}
-                      type='radio'
-                      name='bio'
-                      id='Other'
-                    />
-                    <label htmlFor='Other'>Other</label>
-                    <br />
-                  </div>
-                  <input
-                    onChange={inputsHandler}
-                    className='text-dark'
-                    type='number'
-                    name='tomato'
-                    id='tomato'
-                    defaultValue={25}
-                  />
-                </form>
-              </div>
-              <div className='modal-footer bg-secondary text-white'>
-                <button
-                  type='button'
-                  className='btn btn-secondary'
-                  data-bs-dismiss='modal'
-                >
-                  Close
-                </button>
-                <button
-                  type='submit'
-                  className='btn btn-primary'
-                >
-                  Save
-                </button>
-              </div>
-            </div>
+      <div className="form-design d-flex justify-content-center">
+        <form className="p-3" action="" onSubmit={handleSubmit}>
+          <div>
+            {console.log(inputRef)}
+            <input
+              type="text"
+              placeholder="your task..."
+              name="task"
+              onChange={inputsHandler}
+              ref={inputRef}
+              
+            />{" "}
+            <br />
+            <input
+              value={inputField.bio}
+              onChange={(e) => inputsHandler(e)}
+              defaultChecked
+              type="radio"
+              name="bio"
+              id="home"
+            />
+            <label htmlFor="home">Home</label>
+            <input
+              value={inputField.bio}
+              onChange={(e) => inputsHandler(e)}
+              type="radio"
+              name="bio"
+              id="Office"
+            />
+            <label htmlFor="Office">Office</label>
+            <input
+              value={inputField.bio}
+              onChange={(e) => inputsHandler(e)}
+              type="radio"
+              name="bio"
+              id="Other"
+            />
+            <label htmlFor="Other">Other</label>
+            <br />
           </div>
-        </div>
+          <input
+          ref={inputRef}
+         
+            className="text-dark"
+            type="number"
+            name="tomato"
+            id="tomato"
+            
+          />
+          <div>
+            <button 
+             className="btn btn-outline-primary"
+             type="submit">Start</button>
+          </div>
+        </form>
       </div>
     );
   };
   const Task = () => {
     return (
-      <div className='d-flex justify-content-center'>
-        <div
-          id='taskText'
-          className='p-3 mb-2 bg-light text-dark '
-        >
+      <div className="d-flex justify-content-center">
+        <div id="taskText" className="p-3 mb-2 bg-light text-dark ">
           reactjs
         </div>
       </div>
@@ -157,15 +123,49 @@ export default function Study() {
   };
   const Timer = () => {
     return (
-      <>
+      <div className="row">
         <div>
-          <h2 className='text-center'>Timer</h2>
+          <h2 className="text-center">Timer</h2>
         </div>
-        <div
-          id='show-Timer'
-          className='text-center '
-        >
-          <h1 className='font-weight-bold display-1'>25:00</h1>
+        <div id="show-Timer" className="text-center ">
+          <h1 className="font-weight-bold display-1">25:00</h1>
+        </div>
+      </div>
+    );
+  };
+  const Feedback = () => {
+    return (
+      <>
+        <div className="d-flex justify-content-center feedback">
+          <div className="item-center">
+            <label htmlFor="hieuQua">Mức độ hiệu quả(1-10)</label>
+          </div>
+          <div className=" py-2">
+            <select name="score" id="score">
+              <option value="1">1(Không hiệu quả)</option>
+              <option value="2">2(Không ăn thua)</option>
+              <option value="3">3(qua chuyện)</option>
+              <option value="4">4(Tàm tạm)</option>
+              <option value="5">5(Trung Bình)</option>
+              <option value="6">6(Vừa)</option>
+              <option value="7" selected>
+                7(Khá)
+              </option>
+              <option value="8">8(Tốt)</option>
+              <option value="9">9(Hảo Hảo)</option>
+              <option value="10">10(Tuyệt vời)</option>
+            </select>
+          </div>
+          <div>
+            <textarea
+              className="offset-2 "
+              name="notes"
+              id="note"
+              cols="40"
+              rows="3"
+              placeholder="Notes your Feedback"
+            ></textarea>
+          </div>
         </div>
       </>
     );
@@ -173,18 +173,16 @@ export default function Study() {
   const Perfoment = () => {
     return (
       <form>
-        <div className='text-center d-flex justify-content-center pt-4'>
-          <div id='HieuSuat'>
-            <div className='pb-3'>
-              <select class='custom-select bg-dark text-light'>
+        <div className="text-center d-flex justify-content-center pt-4">
+          <div id="HieuSuat">
+            <div className="pb-3">
+              <select class="custom-select bg-dark text-light">
                 <option selected>Select perfoment</option>
-                <option value='1'>Bab</option>
-                <option value='2'>Medium</option>
-                <option value='3'>Great</option>
+                <option value="1">Bab</option>
+                <option value="2">Medium</option>
+                <option value="3">Great</option>
               </select>
             </div>
-           
-           
           </div>
         </div>
       </form>
@@ -192,24 +190,24 @@ export default function Study() {
   };
   const Toogle = () => {
     return (
-      <div className='modeCSS'>
+      <div className="modeCSS">
         <div
           onClick={handleClick}
-          className={`toogle${toggled ? ' night' : ''}`}
+          className={`toogle${toggled ? " night" : ""}`}
         >
           <div
             onClick={() => {
-              hideToogleMode();
+              hideShowFeeback();
               hideTimer();
             }}
-            className='notch'
+            className="notch"
           ></div>
         </div>
       </div>
     );
   };
-  const hideToogleMode = () => {
-    setShowTooleMode(!showTooleMode);
+  const hideShowFeeback = () => {
+    setShowFeeback(!showFeeback);
   };
   const hideTimer = () => {
     setShowTimer(!showTimer);
@@ -217,125 +215,16 @@ export default function Study() {
 
   return (
     <>
-          <StudyAndMode />
-          <Task />
-      {showTooleMode ? (
-        <>
-          <div className='d-flex justify-content-center p-4'>
-            <button
-              type='button'
-              className='btn btn-secondary  '
-              data-bs-toggle='modal'
-              data-bs-target='#exampleModal'
-              aria-expanded='false'
-              onClick={() => setShowModal(true)}
-            >
-              Add Task
-            </button>
-          </div>
-          {showModal ? (
-            <div>
-              <div
-                className='modal fade '
-                id='exampleModal'
-                tabIndex='-1'
-                aria-labelledby='exampleModalLabel'
-                aria-hidden='true'
-              >
-                <div className='modal-dialog '>
-                  <div className='modal-content'>
-                    <div className='modal-header bg-secondary text-white'>
-                      <h5
-                        className='modal-title'
-                        id='exampleModalLabel'
-                      >
-                        new task
-                      </h5>
-                      <button
-                        type='button'
-                        className='btn-close'
-                        data-bs-dismiss='modal'
-                        aria-label='Close'
-                      ></button>
-                    </div>
-                    {/* body */}
-                    <div className='modal-body bg-info text-dark'>
-                      <form
-                        className=' p-3'
-                        action=''
-                        onSubmit={(e) => handleSubmit(e)}
-                      >
-                        <div>
-                          <input
-                            type='text'
-                            placeholder='your task...'
-                            name='task'
-                            onChange={(e) => inputsHandler(e)}
-                          />{' '}
-                          <br />
-                          <input
-                            onChange={inputsHandler}
-                            defaultChecked
-                            type='radio'
-                            name='bio'
-                            id='home'
-                          />
-                          <label htmlFor='home'>Home</label>
-                          <input
-                            onChange={inputsHandler}
-                            type='radio'
-                            name='bio'
-                            id='Office'
-                          />
-                          <label htmlFor='Office'>Office</label>
-                          <input
-                            onChange={inputsHandler}
-                            type='radio'
-                            name='bio'
-                            id='Other'
-                          />
-                          <label htmlFor='Other'>Other</label>
-                          <br />
-                        </div>
-                        <input
-                          onChange={inputsHandler}
-                          className='text-dark'
-                          type='number'
-                          name='tomato'
-                          id='tomato'
-                          defaultValue={25}
-                        />
-                      </form>
-                    </div>
-                    <div className='modal-footer bg-secondary text-white'>
-                      <button
-                        type='button'
-                        className='btn btn-secondary'
-                        data-bs-dismiss='modal'
-                      >
-                        Close
-                      </button>
-                      <button
-                        type='submit'
-                        className='btn btn-primary'
-                      >
-                        Save
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ) : null}
-        </>
-         
-      ) : null}
+      <StudyAndMode />
       {showTimer ? (
         <>
+          <h3 className="text-center">{inputField.task}</h3>
           <Timer />
           {/* <Perfoment /> */}
         </>
-      ) : null}
+      ) : (
+        <FormTask />
+      )}
     </>
   );
 }
