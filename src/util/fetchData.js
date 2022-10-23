a04ca53edbdc612fc5f51be878e672e1283b1602
T.Anh <langthambca@gmail.com>
2022-10-23 15:08:00 +0700

    fix prefix

--- src/util/fetchData.js
+++ src/util/fetchData.js
@@ -1,4 +1,5 @@
-export const PREFIX = 'http://localhost:3333';
+export const PREFIX = 'https://dark-teal-squirrel-belt.cyclic.app';
+// export const PREFIX = 'http://localhost:3333';
 
 //GET REQUEST
 export const getData = async (path, token) => {
